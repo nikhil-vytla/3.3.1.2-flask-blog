dde99db04eff5f5cb3ff6213dfe0736949295465
efrabaez <e.german@hotmail.com>
2021-06-09 10:42:09 -0500

    Add dynamic bg to home page

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 import os
 from flask import Flask, render_template, send_from_directory
 from dotenv import load_dotenv
-
+from .backgrounds import get_random_background
 load_dotenv()
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html',
+                           title="MLH Fellow",
+                           url=os.getenv("URL"),
+                           random_background=get_random_background())
 
 
 if __name__ == '__main__':
--- app/backgrounds.py
+++ app/backgrounds.py
@@ -2,10 +2,11 @@
 from random import randint, random
 
 
-scss_files = ['bubbles.scss', 'purple.scss', 'stars.scss']
+scss_files = ['bubbles.css', 'purple.css', 'stars.css']
 
 def get_random_background():
     random_index = randint(0, len(scss_files) - 1)
-    return "{{ url_for('static',filename='styles/backgrounds/{" + scss_files[random_index] +  "}') }}"
+    #return "{{ url_for('static',filename='styles/backgrounds/{" + scss_files[random_index] +  "}') }}"
+    return scss_files[random_index]
 
 print(get_random_background())
