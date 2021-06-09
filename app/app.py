2aec0ed678fb008ebc9e10403c0afc158483db92
Nikhil Vytla <nvawesomeguy@gmail.com>
2021-06-09 12:52:56 -0400

    updated custom profile template

--- app/app.py
+++ app/app.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
+@app.route('/profile2')
+def profile2():
+    return render_template('profile2.html', name="John Doe", interests="production engineering!", location="New York, NY, USA", url=os.getenv("URL"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
