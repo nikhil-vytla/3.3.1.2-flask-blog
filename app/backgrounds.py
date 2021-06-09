38ef29bff4c0178028737cace5bd194f4715d37e
Guillermo <Sanguillermo98@gmail.com>
2021-06-08 19:14:28 -0700

    Updated random-backgrounds

--- app/backgrounds.py
+++ app/backgrounds.py
@@ -6,6 +6,6 @@ scss_files = ['bubbles.scss', 'purple.scss', 'stars.scss']
 
 def get_random_background():
     random_index = randint(0, len(scss_files) - 1)
-    return scss_files[random_index]
+    return "{{ url_for('static',filename='styles/backgrounds/{" + scss_files[random_index] +  "}') }}"
 
 print(get_random_background())
