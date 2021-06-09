d4c0751f0e85dd976d38181304695d099507b7e1
Guillermo <Sanguillermo98@gmail.com>
2021-06-08 18:49:36 -0700

    Added random-background generator

---
+++ app/random-background.py
@@ -0,0 +1,11 @@
+
+from random import randint, random
+
+
+scss_files = ['bubbles.scss', 'purple.scss', 'stars.scss']
+
+def get_random_background():
+    random_index = randint(0, len(scss_files) - 1)
+    return scss_files[random_index]
+
+print(get_random_background())
